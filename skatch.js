610f8f5da785bbc5b1f9ea237c2fa350a66be26a
Antonioeduard <00001245032124sp@al.educacao.sp.gov.br>
2024-08-06 15:05:25 -0300

    adicionamdo arquivos js e cs

---
+++ skatch.js
@@ -0,0 +1,15 @@
+let cor;
+
+function setup() {
+  createCanvas(400, 400);
+  background("black");
+}
+function setup() {
+  createCanvas(400, 400);
+  background("black");
+  cor = "red";
+}
+function draw() {
+  fill(cor);
+  circle(200,200,50);
+}
